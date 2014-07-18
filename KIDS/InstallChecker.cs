ad54ad79a468762ac53287236f8358e488fe3986
ferram4 <ferram40@gmail.com>
2014-07-17 21:38:08 -0400

    Add Compat and Install Checkers

---
+++ KIDS/InstallChecker.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * InstallChecker, originally by Majiir
+ * Released into the public domain using a CC0 Public Domain Dedication: http://creativecommons.org/publicdomain/zero/1.0/
+ */
+
+using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+
+namespace KerbalIspDifficultyScaler
+{
+    [KSPAddon(KSPAddon.Startup.MainMenu, true)]
+    internal class InstallChecker : MonoBehaviour
+    {
+        protected void Start()
+        {
+            var assemblies = AssemblyLoader.loadedAssemblies.Where(a => a.assembly.GetName().Name == Assembly.GetExecutingAssembly().GetName().Name).Where(a => a.url != "KerbalIspDifficultyScaler/Plugins");
+            if (assemblies.Any())
+            {
+                var badPaths = assemblies.Select(a => a.path).Select(p => Uri.UnescapeDataString(new Uri(Path.GetFullPath(KSPUtil.ApplicationRootPath)).MakeRelativeUri(new Uri(p)).ToString().Replace('/', Path.DirectorySeparatorChar)));
+                PopupDialog.SpawnPopupDialog("Incorrect KIDS Installation", "KIDS has been installed incorrectly and will not function properly. All KIDS files should be located in KSP/GameData/KerbalIspDifficultyScaler. Do not move any files from inside the KIDS folder.\n\nIncorrect path(s):\n" + String.Join("\n", badPaths.ToArray()), "OK", false, HighLogic.Skin);
+            }
+        }
+    }
+    
+}
